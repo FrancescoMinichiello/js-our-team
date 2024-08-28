05e26ae4b7e49e5e2ea946afc1fba152327a3145
FrancescoMinichiello <fraps2278@gmail.com>
2024-08-28 15:08:13 +0200

    Scaletta

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+1- recuperare gli elementi dal dom
+2- inizilizzare l'array
+3- inserire nell'array i dati forniti (nome, ruolo e foto)
+4- creare un ciclo per girare nell'array
+5- stampare in console
+6- stampare nel DOM
+- per stampare nel dom abbiamo bisogno degli elementi che ci siamo recuperati al 
+(nr-1)
+*/
